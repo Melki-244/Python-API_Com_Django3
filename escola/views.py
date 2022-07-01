fe58f1cd7e6c683eebd2002706b306876492aa18
Melki-244 <melkicavalcante244@gmail.com>
2022-07-01 19:41:04 -0300

    Fix Identation

--- escola/views.py
+++ escola/views.py
@@ -2,10 +2,12 @@ from rest_framework import viewsets
 from escola.models import Aluno, Curso
 from escola.serializer import AlunoSerializer, CursoSerializer
 
+
 class AlunosViewSet(viewsets.ModelViewSet):
-     """Exibindo Todos Os Alunos"""
-     queryset = Aluno.objects.all()
-     serializer_class = AlunoSerializer
+    """Exibindo Todos Os Alunos"""
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+
 
 class CursosViewSet(viewsets.ModelViewSet):
     """Exibindo Todos Os Cursos"""
